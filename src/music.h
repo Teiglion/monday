66a91e719e0f9e51c31b7292d8bc45bba46f3ca7
Teiglion <arnoriandark@gmail.com>
2026-09-12 14:43:22 +0300

    music fix

--- src/music.h
+++ src/music.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 
 extern const uint16_t menu_theme[];
-extern const uint16_t run_theme[];
 extern const uint16_t level_complete[];
 extern const uint16_t win_theme[];
 extern const uint16_t fail_theme[];
